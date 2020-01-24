5f80860714c14be9144898955d2c8fb1e9e616d1
Vitor de Souza <vitordesolza@gmail.com>
2020-01-23 23:09:10 -0300

    Adição das primeiras ideias de fabricas de entidades

---
+++ EntityProjects/Gepa.Server.Entity/Attributes/ManufacturerAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Gepa.Server.Entity.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Method | AttributeTargets.Field)]
+    internal class ManufacturerAttribute : Attribute
+    {
+    }
+}
---
+++ EntityProjects/Gepa.Server.Entity/Exceptions/ClassHasNotManufacturableAttributeException.cs
@@ -0,0 +1,31 @@
+﻿using Gepa.Server.Entity.Attributes;
+using System;
+using System.Runtime.Serialization;
+
+namespace Gepa.Server.Entity.Exceptions
+{
+    public class ClassHasNotManufacturableAttributeException : Exception
+    {
+        public static readonly string DEFAULT_ERROR_MESSAGE_FORMAT = $@"A classe {{0}} não possui nenhuma propriedade ou método com o atributo {nameof(ManufacturerAttribute)}";
+
+        public ClassHasNotManufacturableAttributeException(string message) : base(message)
+        {
+        }
+
+        public ClassHasNotManufacturableAttributeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public ClassHasNotManufacturableAttributeException()
+        {
+        }
+
+        public ClassHasNotManufacturableAttributeException(Type classWithoutManufacturerAttribute) : base(string.Format(DEFAULT_ERROR_MESSAGE_FORMAT, classWithoutManufacturerAttribute.Name))
+        {
+        }
+
+        protected ClassHasNotManufacturableAttributeException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
